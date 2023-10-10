dd29358228aeaff36d999a7642caf3ff402817ca
Carlos Ortiz <carlosortizur@gmail.com>
2023-10-10 11:55:40 -0500

    whatsapp config update

--- src/util/timing.py
+++ src/util/timing.py
@@ -4,7 +4,6 @@ def timing(func):
     def wrapper(*args, **kwargs):
         start_time  = time.time()
         result      = func(*args, **kwargs)
-        print(result)
         end_time    = time.time()
         return ( result , (end_time - start_time) * 1000 )
     return wrapper
